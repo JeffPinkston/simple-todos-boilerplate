fcd4d6d80c0686a498fd1f9a3484e75fb40daa9a
Jeff Pinkston <jeff.pinkston@pearson.com>
2016-07-07 11:06:44 -0500

    DatePicker Style fix

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,11 +4,7 @@ import moment from 'moment';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
-import { DropdownButton } from 'react-bootstrap';
-import { MenuItem } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { Table } from 'react-bootstrap';
-import { Panel } from 'react-bootstrap';
+import { DropdownButton, MenuItem, Button, Table, Panel } from 'react-bootstrap';
 import  DatePicker  from 'react-datepicker';
 
 import { Tasks } from '../api/tasks.js';
@@ -46,6 +42,21 @@ class App extends Component {
 
     //clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  handleAlbumSubmit(event) {
+    event.preventDefault();
+
+    const name = ReactDOM.findDOMNode(this.refs.albumName).value.trim();
+    const data = ReactDOM.findDOMNode(this.refs.albumDate).value.trim();
+    Albums.insert({
+      name: this.state.albumName,
+      localtion: '',
+      date: this.state.selectedDate
+    });
+
+    ReactDOM.findDOMNode(this.refs.albumName).value = '';
+    ReactDOM.findDOMNode(this.refs.albumDate).value = '';
   }
 
   toggleHideCompleted() {
@@ -143,6 +154,7 @@ class App extends Component {
           onChange={this.handleNameChange.bind(this)}
           />
         <DatePicker
+          ref="albumDate"
           selected={this.state.selectedDate}
           onChange={this.handleDateChange.bind(this)}/>
         </Panel>
